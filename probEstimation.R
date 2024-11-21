3b2fa4854d28ca65df06549159e751e1d95b8c4d
ajawid158 <ajawid@auaf.edu.af>
2024-11-21 13:21:04 +0430

    ProbUpdate

--- probEstimation.R
+++ probEstimation.R
@@ -5,12 +5,11 @@ View(t)
 
 plot(ecdf(t$T))
 
-#Pr(T<12)
+#Pr(T<13)=14%
 ecdf(t$T)(13)
-ecdf(t$T)(16)
 
-#Pr(T>13)=1-Pr(T<13)
-1-ecdf(t$T)(13)
+#Pr(T>16)
+1-ecdf(t$T)(16)
 
-#Pr(13<T<16)=Pr(T<16)-Pr(T<13)
+#Pr(13<T<16)
 ecdf(t$T)(16)-ecdf(t$T)(13)
